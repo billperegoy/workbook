fb7fab5b5cd395dbbe40941bdac0311170023e02
billperegoy <bill@billperegoy.com>
2014-11-08 15:14:00 -0500

    Initial rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Workbook::Application.config.secret_key_base = '383b6991d346be5a99182780596fe380cdbd134e668bbee0bc65bcad51184ce39e61c58e82fe07542acb928b7f7587d50ba21497baed9ab99e54c260a72847a8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Workbook::Application.config.session_store :cookie_store, key: '_workbook_session'
