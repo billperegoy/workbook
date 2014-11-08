0ee5fbab2f1119bde2242385d3596e71aaf598c9
billperegoy <bill@billperegoy.com>
2014-11-08 16:56:16 -0500

    Updated seeds file and gitignore.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+User.delete_all
 User.create(username: 'sheep', hashed_password: 'abcd', email: 'sheep@sheeprocks.com', role: 'admin')
 User.create(username: 'joe', hashed_password: 'abcd', email: 'sheep@sheeprocks.com', role: 'admin')
 User.create(username: 'paul', hashed_password: 'abcd', email: 'sheep@sheeprocks.com', role: 'admin')
