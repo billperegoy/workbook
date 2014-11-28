7b3b60c5c5f1a78fa3625dc0fac5875be949a7ae
billperegoy <bill@billperegoy.com>
2014-11-27 23:07:41 -0500

    Added static pages. Linked main navigation items to pages.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Workbook::Application.routes.draw do
   get '/store_items' => 'store_items#index'
   get '/store_items/:id/add_to_cart' => 'store_items#add_to_cart', as: 'add_to_cart'
 
+  get '/static_pages/about' => 'static_pages#about', as: 'about'
+  get '/static_pages/contact' => 'static_pages#contact', as: 'contact'
+
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy'
