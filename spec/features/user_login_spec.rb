2241553898719088a5ca8aa79f169546b45f941b
billperegoy <bill@billperegoy.com>
2015-01-04 15:24:18 -0500

    Fixed failing feature test.

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -4,9 +4,9 @@ describe "login process" do
   it ": user can login", js: true do
     visit '/'
     click_link('log in')
-    expect(page).to have_content('Please Log In')
-    fill_in "Username", with: 'sheep'
-    fill_in "Password", with: 'password'
+    expect(page).to have_content('log in')
+    fill_in "login-username", with: 'sheep'
+    fill_in "login-password", with: 'password'
     #click_button('Log In')
     #expect(page).to have_content('My Dashboard')
   end
